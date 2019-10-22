90c5841850c8467550a231ed2e177cf56fb3e60f
Junhyuk Kim <okjcd123@naver.com>
2019-10-22 20:55:43 +0900

    json testing

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+let getData = () =>{
+}
+let searchData = () =>{
+
+    let data = getData();
+    
+    var div = document.createElement('div');
+
+
+    console.log("start");
+
+    document.querySelector('cardSet').append(div);
+    console.log("done");
+}
